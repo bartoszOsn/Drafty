4d5648993ae9cc38d16fb588f3e16d358b4bc92e
bartoszOsn <osinskibartosz24@gmail.com>
2021-01-23 15:14:51 +0100

    changing password is now fully functional

--- server/routes/password.js
+++ server/routes/password.js
@@ -7,6 +7,7 @@ const pug = require('pug');
 const path = require('path');
 const UserDetails = require('./../models/userDetail');
 const { mail } = require('./../../secret');
+const {isPasswordValid} = require('./../../shared/validation');
 var router = express.Router();
 
 
@@ -63,8 +64,87 @@ router.post('/forgot', async (req, res, next) => {
     });
 });
 
-router.get('/forgot/:user/:token', (req, res, next) => {
+router.get('/forgot/:user/:token', async (req, res, next) => {
+    const userID = req.params.user;
+    const token = req.params.token;
+    let correctId = true;
+    let user
+    try {
+        user = await UserDetails.findById(userID);
+    }
+    catch(ex) {
+        correctId = false;
+    }
+    if(!correctId || !user || user.token != token) {
+        return res.render('account/info', {
+            msgHeader: 'This link is incorrect.',
+            link: '/password/forgot',
+            linksText: 'Click here to send mail with correct link for changing password'
+        });
+    }
+    if(user.tokenExpiration < Date.now()) {
+        return res.render('account/info', {
+            msgHeader: 'Your link has expired.',
+            link: '/password/forgot',
+            linksText: 'Click here to send mail with new link for changing password'
+        });
+    }
     return res.render('account/new_password', {oldPassword: false});
+});
+
+router.post('/forgot/:user/:token', async (req, res, next) => {
+    const userID = req.params.user;
+    const token = req.params.token;
+    const newPassword = req.body.newPassword;
+    let correctId = true;
+    let user
+    try {
+        user = await UserDetails.findById(userID);
+    }
+    catch(ex) {
+        correctId = false;
+    }
+    if(!correctId || !user || user.token === null || user.token != token) {
+        return res.render('account/info', {
+            msgHeader: 'This link is incorrect.',
+            link: '/password/forgot',
+            linksText: 'Click here to send mail with correct link for changing password.'
+        });
+    }
+    if(user.tokenExpiration < Date.now()) {
+        return res.render('account/info', {
+            msgHeader: 'Your link has expired.',
+            link: '/password/forgot',
+            linksText: 'Click here to send mail with new link for changing password.'
+        });
+    }
+    let passwordValidation = isPasswordValid(newPassword);
+    if(!passwordValidation.result) {
+        return res.render('account/info', {
+            msgHeader: 'Your password is too weak.',
+            message: 'Password must be at least 8 characters long, and have at least 2 capital letters and 2 numbers.',
+            link: req.originalUrl,
+            linksText: 'Click here to type another password.'
+        });
+    }
+    try {
+        await user.setPassword(newPassword);
+        user.token = null;
+        await user.save();
+        return res.render('account/info', {
+            msgHeader: 'Password changed successfully.',
+            link: '/login',
+            linksText: 'You can log in now.'
+        });
+    }
+    catch(ex) {
+        return res.render('account/info', {
+            msgHeader: 'Something went wrong.',
+            link: '/password/forgot',
+            linksText: 'Click here to send another link and try again.'
+        });
+    }
+    
 });
 
 module.exports = router;
